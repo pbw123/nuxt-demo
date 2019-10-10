3097129dabf2b3f25289e1d6fd517d89c5a4698e
Joker <2635225112.com>
2019-10-10 09:34:56 +0800

    动态路由

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,9 @@
     <nuxt-link to="/">首页</nuxt-link>
     <nuxt-link to="/user">用户index</nuxt-link>
     <nuxt-link to="/user/one">用户one</nuxt-link>
+    <nuxt-link to="/user/123">动态路由</nuxt-link>
+    <nuxt-link to="/users/java">动态路由2</nuxt-link>
+
 
     <nuxt />
   </div>
