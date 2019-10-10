b2b7268fcb806a0ac3bf4b9f22fbf7bcc6ec3e65
Joker <2635225112.com>
2019-10-10 13:36:00 +0800

    使用sass

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="background: #3b8070">
+  <div class="title">
     <div>我的博客导航栏在这里</div>
     <nuxt/>
   </div>
@@ -11,6 +11,6 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+@import "../assets/scss/index.scss";
 </style>
